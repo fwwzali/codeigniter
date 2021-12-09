b0ec8ba1f1261e2799ae1fb2559f04bac5908e49
ewez <ewezali@gmail.com>
2021-12-09 14:49:55 +0700

    numrows

--- application/models/M_login.php
+++ application/models/M_login.php
@@ -10,4 +10,14 @@ class M_login extends CI_Model
 	{
 		parent::__construct();
 	}
+
+	function check_login($data)
+	{
+		//cek apakah data username dan password itu sama
+		$sql = "SELECT * FROM karyawan
+				WHERE username = ? AND password = md5(?)";
+		$res = $this->db->query($sql,array($data['username'],$data['password']));
+		return $res->num_rows();
+
+	}
 }
